bcd61fa852d91baff71e8728adc8a6054f8ffadf
Sagar Guhe <sagar@localhost.localdomain>
2017-09-12 17:44:57 +0530

    fix bug in server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 import * as path from 'path';
 
 const app = express();
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
 console.log('Server listening on port no. 8080');
