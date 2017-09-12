bc3ef4dd5c86abcf1b46aac55609b58c185575d7
Sagar Guhe <sagar@localhost.localdomain>
2017-09-12 17:11:44 +0530

    prepare for heroku

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Server listening on port no. 8080');
